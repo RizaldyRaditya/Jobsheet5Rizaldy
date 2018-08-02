2e4431e0f822609fccb58d3ad6d97cafd4de4362
Rudi Erwanto <Rudi Erwanto@DESKTOP-86HM3LO>
2018-08-02 11:35:57 +0700

    Initial Commmit

---
+++ src/Latihan/OperatorAritmatika2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author Rizaldy Raditya Erwanto
+ */
+public class OperatorAritmatika2 {
+    public static void main(String[] args) {
+        double x = 10;
+        int y = 3;
+        double z = 2.5;
+        
+        double a = 0;
+        double b = 0;
+        //Operasi Aritmatika
+        a = x / y ;
+        b = x / z ;
+        System.out.println("Hasil dari"+a);
+        System.out.println("Hasil dari"+b);
+
+    }
+}
